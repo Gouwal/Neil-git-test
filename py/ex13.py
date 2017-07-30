d951675ddf31b909ff95509f2bcf88218c02ae2a
Gouwal <Neil_zhang@126.com>
2017-07-30 17:26:28 +0800

    py

---
+++ py/ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The scrip is call:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+nummber = raw_input("which nummber you like ? ")
+print "You like nummber %r." % (nummber)
